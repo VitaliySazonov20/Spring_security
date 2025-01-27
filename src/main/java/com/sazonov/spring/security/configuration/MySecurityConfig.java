51113feeb328e81414e8b1339af4647abc163170
V.Sazonov <VitaliySazonov20@gmail.com>
2025-01-27 16:35:07 +0400

    added views after authentication

--- src/main/java/com/sazonov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/sazonov/spring/security/configuration/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.sazonov.spring.security.configuration;
 
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,7 +10,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
+@Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
 
---
+++ src/main/webapp/WEB-INF/view/view_for_all_emps.jsp
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<h2>  Information for all employees </h2>
+
+<input type="button" value="Salary"
+    onclick="window.location.href = 'hr_info'">
+   Only for HR staff
+
+<br><br>
+
+<input type="button" value="Performance"
+    onclick="window.location.href = 'manager_info'">
+   Only for Management Staff
+</body>
+
+</html>
