17ec5f63c7b51c753e82a681edd75217130dda82
V.Sazonov <VitaliySazonov20@gmail.com>
2025-01-28 15:30:32 +0400

    passwords stored in db

--- src/main/java/com/sazonov/spring/security/configuration/MyConfig.java
+++ src/main/java/com/sazonov/spring/security/configuration/MyConfig.java
@@ -1,12 +1,16 @@
 package com.sazonov.spring.security.configuration;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.sazonov.spring.security")
@@ -21,5 +25,15 @@ public class MyConfig {
         internalResourceViewResolver.setPrefix("/WEB-INF/view/");
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() throws PropertyVetoException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("org.postgresql.Driver");
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUser("postgres");
+        dataSource.setPassword("1");
+        return dataSource;
     }
 }
--- src/main/java/com/sazonov/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/sazonov/spring/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sazonov.spring.security.configuration;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,28 +9,40 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
 
+    @Autowired
+    DataSource dataSource;
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withDefaultPasswordEncoder()
+//                .username("Vitaliy")
+//                .password("Sazonov")
+//                .roles("EMPLOYEE").build());
+//        manager.createUser(User.withDefaultPasswordEncoder()
+//                .username("John")
+//                .password("Doe")
+//                .roles("HR").build());
+//        manager.createUser(User.withDefaultPasswordEncoder()
+//                .username("Jane")
+//                .password("Doe")
+//                .roles("MANAGER","HR").build());
+//        return manager;
+//    }
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder()
-                .username("Vitaliy")
-                .password("Sazonov")
-                .roles("EMPLOYEE").build());
-        manager.createUser(User.withDefaultPasswordEncoder()
-                .username("John")
-                .password("Doe")
-                .roles("HR").build());
-        manager.createUser(User.withDefaultPasswordEncoder()
-                .username("Jane")
-                .password("Doe")
-                .roles("MANAGER","HR").build());
+    public JdbcUserDetailsManager userDetailsManager(){
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
         return manager;
     }
 
